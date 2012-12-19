6afa517100ac4645bd392a6def08beecfb60974d
David THENON <sveetch@gmail.com>
2012-12-19 15:57:58 +0100

    First commit

---
+++ optimus_foundation/__init__.py
@@ -0,0 +1,40 @@
+"""
+This is an Optimus template project for Foundation, the version will follow the version of Foundation
+"""
+__version__ = "3.2.3"
+
+# Directory structure to create
+DIRECTORY_STRUCTURE = [
+    # list(dir_name[, children_dir_list])
+    [
+        'sources',
+        [
+            ['js'],
+            ['css'],
+            ['scss'],
+            ['images'],
+            ['templates'],
+        ]
+    ]
+]
+
+# Script template files
+# NOTE: Files are rendered with the ``String.format()`` method, so remember to double 
+#       all your '{' and '}', else they will be interpreted as format variable, and 
+#       probably raise a Key error
+SCRIPT_FILES = [
+    ['requirements.txt', 'requirements.txt'],
+    ['gemsets.txt', 'gemsets.txt'],
+    ['README.rst', 'README.rst'],
+    # Default scripts
+    ['scripts/__init__.py.tpl', '__init__.py'],
+    ['scripts/settings.py.tpl', 'settings.py'],
+    ['scripts/prod_settings.py.tpl', 'prod_settings.py'],
+    ['scripts/pages.py.tpl', 'pages.py'],
+    ['scripts/config.rb.tpl', 'config.rb'],
+    # Default sources
+    ['sources/templates/base.html', 'sources/templates/base.html'],
+    ['sources/templates/index.html', 'sources/templates/index.html'],
+    ['sources/templates/readme.html', 'sources/templates/readme.html'],
+    ['sources/css/screen.css', 'sources/css/screen.css'],
+]
